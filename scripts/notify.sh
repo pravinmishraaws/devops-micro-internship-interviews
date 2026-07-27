cd5ff0791658a303e35227f835282a0160ea557a
Ebenezer Ndubuisi <uchennaebenezer01@gmail.com>
2026-07-27 17:52:32 +0100

    add notification script

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# demo only - placeholder  notification script, no credentials used 
-echo "Notification script running" 
+# demo only - placeholder notification script, no credentials used
+echo "Notification script running"
